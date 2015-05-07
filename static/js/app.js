74000e1d3088ce5b264f89c37bded8dfcef59af8
Luis Eduardo <yayohack@hotmail.com>
2015-05-06 19:49:00 -0500

    acordeon

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,16 @@
-angular.module('carfix', [])
+angular.module('carfix', ['ui.bootstrap'])
 .controller('getData', function ($scope, $http) {
 	$scope.sortType     = 'name'; // set the default sort type
  	$scope.sortReverse  = false;  // set the default sort order
   	$scope.searchClient   = '';     // set the default search/filter term
   	$scope.errors = [];
     $scope.msgs = [];
+    $scope.error = false;
 
 	$http.get('http://carfix-rodriguezramirez.rhcloud.com/clients')
 	.success(function(data, status, headers, config) {
 		$scope.clientes = data;
+        console.log($scope.clientes);
 	  })
 	  .error(function(data, status, headers, config) {
 	  	$scope.clientes = data;
@@ -21,12 +23,16 @@ angular.module('carfix', [])
         	"phone"   : $scope.phone,
         	"email"   : $scope.email
         	};
-
-        	$scope.errors.splice(0, $scope.errors.length);
+            $scope.errors.splice(0, $scope.errors.length);
             $scope.msgs.splice(0, $scope.msgs.length);
- 
-            $http.post('http://carfix-rodriguezramirez.rhcloud.com/clients', client)
-            	.success(function(data, status, headers, config) {
+
+            if($scope.name == undefined || $scope.address == undefined || $scope.phone == undefined){
+                $scope.errors.push("Please fill all fields.");
+                $scope.error = true;
+            }
+            else{
+                $http.post('http://carfix-rodriguezramirez.rhcloud.com/clients', client)
+                .success(function(data, status, headers, config) {
                     $http.get('http://carfix-rodriguezramirez.rhcloud.com/clients')
                     .success(function(data, status, headers, config) {
                         $scope.clientes = data;
@@ -35,6 +41,7 @@ angular.module('carfix', [])
                 .error(function(data, status) {
                         $scope.errors.push(status);
                 });
+            }
       };
 
     $scope.delete = function(name) {
